243c5a4520795d317d3c4817d260ad985488cacf
akshith-3 <gakshithrao1663@gmail.com>
2023-01-09 23:13:11 +0530

    Level-11 initial review_2

--- migrations/20230109174000-add-questionid-in-options.js
+++ migrations/20230109174000-add-questionid-in-options.js
@@ -4,20 +4,20 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn("options", "questionId", {
-      type: Sequelize.DataTypes.INTEGER,
-      onDelete: "CASCADE",
-    });
-
-    await queryInterface.addConstraint("options", {
-      fields: ["questionId"],
-      type: "foreign key",
-      onDelete: "CASCADE",
-      references: {
-        table: "questions",
-        field: "id",
-      },
-    });
+      await queryInterface.addColumn("options", "questionId", {
+        type: Sequelize.DataTypes.INTEGER,
+        onDelete: "CASCADE",
+      });
+  
+      await queryInterface.addConstraint("options", {
+        fields: ["questionId"],
+        type: "foreign key",
+        onDelete: "CASCADE",
+        references: {
+          table: "questions",
+          field: "id",
+        },
+      });
     /**
      * Add altering commands here.
      *
@@ -27,7 +27,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn("options", "questionId");  
+    await queryInterface.removeColumn("options", "questionId");
     /**
      * Add reverting commands here.
      *
